6d2d2b63d1e2a6b466481a9aabfffbf3f33efdb3
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2022-08-17 21:52:04 +0900

    create contract_test

--- tests/solana-cloud-funding.ts
+++ tests/solana-cloud-funding.ts
@@ -1,16 +1,61 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, utils, web3, BN } from "@project-serum/anchor";
+import { PublicKey } from "@solana/web3.js";
 import { SolanaCloudFunding } from "../target/types/solana_cloud_funding";
+import * as assert from "assert";
 
 describe("solana-cloud-funding", () => {
+  // Use a local provider.
+  const provider = anchor.AnchorProvider.local();
+
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SolanaCloudFunding as Program<SolanaCloudFunding>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Cloud Funding Successful", async () => {
+    const [campaign] = await PublicKey.findProgramAddress(
+      [
+        utils.bytes.utf8.encode("CAMPAIGN_DEMO"),
+        provider.wallet.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+    await program.rpc.create("campaign name", "campaign description", {
+      accounts: {
+        campaign,
+        user: provider.wallet.publicKey,
+        systemProgram: web3.SystemProgram.programId,
+      },
+    });
+
+    let account = await program.account.campaign.fetch(campaign);
+    assert.ok(account.name === "campaign name");
+    assert.ok(account.description === "campaign description");
+    assert.ok(account.amountDonated.eq(new anchor.BN(0)));
+
+    // 0.2 * web3.LAMPORTS_PER_SOLは預けるお金
+    await program.rpc.donate(new BN(0.2 * web3.LAMPORTS_PER_SOL), {
+      accounts: {
+        campaign,
+        user: provider.wallet.publicKey,
+        systemProgram: web3.SystemProgram.programId,
+      },
+    });
+
+    account = await program.account.campaign.fetch(campaign);
+    assert.ok(account.amountDonated.eq(new BN(0.2 * web3.LAMPORTS_PER_SOL)));
+
+    // 0.2 * web3.LAMPORTS_PER_SOLは引き出すお金
+    const result = await program.rpc.withdraw(new BN(0.2 * web3.LAMPORTS_PER_SOL), {
+      accounts: {
+        campaign,
+        user: provider.wallet.publicKey,
+      },
+    });
+
+    account = await program.account.campaign.fetch(campaign);
+    assert.ok(account.amountDonated.eq(new anchor.BN(0)));
   });
 });
